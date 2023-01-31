936fd5e4d3c802cc89f5b2b3e33c683528d18509
Diego <juandiegoaguilarok@gmail.com>
2023-01-31 16:15:08 -0500

    Curso de LINQ terminado

---
+++ curso-linq/LinqQueries.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curso_linq
+{
+    public class LinqQueries
+    {
+        private List<Book> librosCollection = new List<Book>();
+
+        public LinqQueries()
+        {
+            using (StreamReader reader = new StreamReader("C:\\Users\\jdaguilar\\Desktop\\juan diego\\Estudio\\C#\\LINQ\\CursoLINQ\\curso-linq\\books.json"))
+            {
+                string json = reader.ReadToEnd();
+                this.librosCollection = System.Text.Json.JsonSerializer.Deserialize<List<Book>>(json, new System.Text.Json.JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+            }
+        }
+       
+        public IEnumerable<Book> TodaLaColeccion()
+        {
+            return librosCollection;
+        }
+        public IEnumerable<Book> BooksAfter2000() 
+        {//extension method
+            //return librosCollection.Where(book => book.PublishedDate.Year > 2000);
+            
+            //query expression
+            return from book in librosCollection where book.PublishedDate.Year > 2000
+                select book;
+        }
+        // books must contain 250 pages and the title the words in Action
+        public IEnumerable<Book> Books250pagesInAction()
+        {
+            //extension method
+            //return librosCollection.Where(book => book.Title.Contains("in Action") && book.PageCount > 250);
+
+            //Query expression
+            return from book in librosCollection
+                where book.Title.Contains("in Action") && book.PageCount > 250
+                select book;
+        }
+
+        public bool AllBooksHaveStatus()
+        {
+            //extension method
+            return librosCollection.All(p => p.Status != string.Empty);
+        }
+
+        public bool BookPublishedIn2005() 
+        {
+            return librosCollection.Any(book => book.PublishedDate.Year == 2005);
+        }
+
+
+        public IEnumerable<Book> BooksPythonCategory() 
+        { //extension method
+            //return librosCollection.Where(book => book.Categories.Contains("Python"));
+
+            //query method 
+            return from book in librosCollection where book.Categories.Contains("Python") select book;
+        }
+
+        public IEnumerable<Book> BooksOrderbyName() 
+        { // extension method
+            return librosCollection.Where(book => book.Categories.Contains("Java")).OrderBy(title => title.Title);
+        }
+
+        public IEnumerable<Book> BooksOrderbyPages()
+        {
+            return librosCollection.Where(book => book.PageCount > 450).OrderByDescending(book => book.PageCount);
+        }
+
+        public IEnumerable<Book> OperatorTake()
+        {
+            return librosCollection.Where(book => book.Categories.Contains("Java"))
+                .OrderByDescending(book => book.PublishedDate).Take (3);
+        }
+
+        public IEnumerable<Book> OperatorSkip()
+        {
+            return librosCollection.Where(book => book.PageCount > 400).Skip(2).Take(2);
+        }
+
+        public IEnumerable<Book> OperatorSelect() 
+        {
+             return librosCollection.Take(3).Select(book => new  Book() {
+                    Title = book.Title,
+                    PageCount = book.PageCount
+            });
+        }
+
+        public int OperatorCount()
+        {
+            return librosCollection.Count(book => book.PageCount >= 200 && book.PageCount <= 500);
+        }
+
+        public DateTime MostOldBook()
+        {
+            return librosCollection.Min(book => book.PublishedDate);
+        }
+
+
+        public int MostPagesMax()
+        {
+            return librosCollection.Max(book => book.PageCount);
+        }
+
+        public Book OpertatorMinby()
+        {
+            return librosCollection.Where(book => book.PageCount > 0).MinBy(page => page.PageCount);
+        }
+
+        public Book OpertatorMaxby()
+        {
+            return librosCollection.MaxBy(book => book.PublishedDate);
+        }
+
+        public int OpertatorSum()
+        {
+            return librosCollection.Where(book => book.PageCount > 0 && book.PageCount <= 500).Sum(book => book.PageCount);
+        }
+
+        public string OperatorAggregate()
+        {
+            return librosCollection.Where(book => book.PublishedDate.Year > 2015).Aggregate("", (TitulosLibros, next) =>
+            {
+                if (TitulosLibros != string.Empty)
+                {
+                    TitulosLibros += " - " + next.Title;
+                }
+                else
+                {
+                    TitulosLibros += next.Title;
+                }
+
+                return TitulosLibros;
+            });
+        }
+
+
+        public double OperatorAverage()
+        {
+            return librosCollection.Average(book => book.Title.Length);
+        }
+
+        public IEnumerable<IGrouping<int, Book>> OperatorGroupby()
+        {
+            return librosCollection.Where(book => book.PublishedDate.Year >= 2000).GroupBy(book => book.PublishedDate.Year);
+        }
+
+        public ILookup<char, Book> OperatorLookup()
+        {
+            return librosCollection.ToLookup(book => book.Title[0], book => book);
+        }
+
+
+        public IEnumerable<Book> BooksWithMoreThan2005withMoreThan500pages()
+        {
+            var booksAfter2005 = librosCollection.Where(book => book.PublishedDate.Year > 2005);
+
+            var bookMore500pages = librosCollection.Where(book => book.PageCount > 500);
+
+            return booksAfter2005.Join(bookMore500pages, book1 => book1.Title, book2 => book2.Title,
+                (book1, book2) => book1);
+
+        }
+    }
+}
+
---
+++ curso-linq/Program.cs
@@ -0,0 +1,113 @@
+﻿namespace curso_linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            LinqQueries queries = new LinqQueries();
+            //All collections
+            //PrintValues(queries.TodaLaColeccion());
+
+            //Books post 2000
+            //PrintValues(queries.BooksAfter2000());
+
+            //Page count > 250 and title must contains in Action
+            //PrintValues(queries.Books250pagesInAction());
+
+            //all books have status
+            //Console.WriteLine($"All books have status: {queries.AllBooksHaveStatus()}");
+
+            //Search if a book or books have been published in 2005 return a bool
+            //Console.WriteLine($"Have been any books published in 2005: {queries.BookPublishedIn2005()} ");
+
+            //books that contains in categories python
+            //PrintValues(queries.BooksPythonCategory());
+
+            //Books that have the categorie of java and order by name
+            //PrintValues(queries.BooksOrderbyName());
+
+            //Books order by pages dsc
+            //PrintValues(queries.BooksOrderbyPages());
+
+            //First 3 books most recent and the categorie has to be Java
+            //PrintValues(queries.OperatorTake());
+
+            //thrid and fourth books with more than 400 pages
+            //PrintValues(queries.OperatorSkip());
+
+            //first three books filtred with select
+            //PrintValues(queries.OperatorSelect());
+
+            //count book with pages between 200 and 500
+            //Console.WriteLine(queries.OperatorCount());
+
+            //Oldest book by publish date
+            //Console.WriteLine($"The oldest book by publish date is {queries.MostOldBook()}");
+
+            //Count of most pages in a book
+            //Console.WriteLine($"The biggest cant of pages in a book is {queries.MostPagesMax()}");
+
+            ////book with the less pages
+            //Book lessPages = queries.OpertatorMinby();
+            //Console.WriteLine($"El libro con menos paginas es {lessPages.Title}");   
+
+            //book with the most recent publish date
+            //Book mostRecent = queries.OpertatorMaxby();
+            //Console.WriteLine($"El libro mas reciente es {mostRecent.Title} \n y su fecha es {mostRecent.PublishedDate}");
+
+            //sum of books  pages of 0 and 500
+            //Console.WriteLine($"La suma de las paginas es {queries.OpertatorSum()}");
+
+            //Concat al the book that are post 2015
+            //Console.WriteLine($"Los libros son: \n {queries.OperatorAggregate()}");
+
+            //Books Mod of chars of the title
+            //Console.WriteLine($"The mod of the title chars is: {queries.OperatorAverage()}");
+
+            //book published  in or after 2000
+            //PrintGroup(queries.OperatorGroupby());
+
+            //books who starts with a letter operator lookup
+            //var dictionary = queries.OperatorLookup();
+            //ImprimirDiccionario(dictionary, 'A');
+
+            // operator join
+            PrintValues(queries.BooksWithMoreThan2005withMoreThan500pages());
+        }
+
+        public static void PrintValues(IEnumerable<Book> booksList)
+        {
+            
+
+            foreach (var book in booksList) 
+            {
+                Console.WriteLine("{0,-60} {1,9} {2,11}",book.Title, book.PageCount, book.PublishedDate.ToShortDateString());
+            }
+        }
+        public static void PrintGroup(IEnumerable<IGrouping<int, Book>> ListadeLibros)
+        {
+            foreach (var grupo in ListadeLibros)
+            {
+                Console.WriteLine("");
+                Console.WriteLine($"Grupo: {grupo.Key}");
+                Console.WriteLine("{0,-60} {1, 15} {2, 15}\n", "Titulo", "N. Paginas", "Fecha publicacion");
+                foreach (var item in grupo)
+                {
+                    Console.WriteLine("{0,-60} {1, 15} {2, 15}", item.Title, item.PageCount, item.PublishedDate.Date.ToShortDateString());
+                }
+            }
+        }
+
+        public static void ImprimirDiccionario(ILookup<char, Book> ListadeLibros, char letra)
+        {
+            Console.WriteLine("{0,-60} {1, 15} {2, 15}\n", "Titulo", "N. Paginas", "Fecha publicacion");
+            foreach (var item in ListadeLibros[letra])
+            {
+                Console.WriteLine("{0,-60} {1, 15} {2, 15}", item.Title, item.PageCount, item.PublishedDate.Date.ToShortDateString());
+            }
+        }
+
+
+    }
+}
